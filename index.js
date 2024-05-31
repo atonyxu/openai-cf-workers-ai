067f2594f5098b7388f127c26412daa3e2d01a85
白糖の正义铃 <31569794+atonyxu@users.noreply.github.com>
2024-05-31 15:10:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,55 +14,55 @@ const { preflight, corsify } = createCors();
 const router = Router({ base: '/v1' });
 
 function extractToken(authorizationHeader) {
-	if (authorizationHeader) {
-		const parts = authorizationHeader.split(' ');
-		if (parts.length === 2 && parts[0] === 'Bearer') {
-			return parts[1];
-		}
-	}
-	return null;
+    if (authorizationHeader) {
+        const parts = authorizationHeader.split(' ');
+        if (parts.length === 2 && parts[0] === 'Bearer') {
+            return parts[1];
+        }
+    }
+    return null;
 }
 // MIDDLEWARE: withAuthenticatedUser - embeds user in Request or returns a 401
 const bearerAuthentication = (request, env) => {
-	const authorizationHeader = request.headers.get('Authorization');
-	// if (!authorizationHeader) {
-	// 	return error(401, 'Unauthorized');
-	// }
-	const access_token = extractToken(authorizationHeader);
-	// if (env.ACCESS_TOKEN !== access_token) {
-	// 	return error(403, 'Forbidden');
-	// }
+    const authorizationHeader = request.headers.get('Authorization');
+    const access_token = extractToken(authorizationHeader);
+    const region = request.headers.get('cf-ipcountry').toUpperCase();
+    const allowed_region = ['CN'];
+
+    if (!allowed_region.includes(region)) {
+        return error(403, 'Forbidden');
+    }
 };
 
 // CORS, see https://itty.dev/itty-router/cors
 router.all('*', preflight);
 
 router
-	.all('*', bearerAuthentication)
-	.post('/chat/completions', chatHandler)
-	.post('/completions', completionHandler)
-	.post('/embeddings', embeddingsHandler)
-	.post('/audio/transcriptions', transcriptionHandler)
-	.post('/audio/translations', translationHandler)
-	.post('/images/generations', imageGenerationHandler)
-	.get('/images/get/:name', getImageHandler)
-	.get('/models', modelsHandler);
+    .all('*', bearerAuthentication)
+    .post('/chat/completions', chatHandler)
+    .post('/completions', completionHandler)
+    .post('/embeddings', embeddingsHandler)
+    .post('/audio/transcriptions', transcriptionHandler)
+    .post('/audio/translations', translationHandler)
+    .post('/images/generations', imageGenerationHandler)
+    .get('/images/get/:name', getImageHandler)
+    .get('/models', modelsHandler);
 
 // 404 for everything else
 router.all('*', () => new Response('404, not found!', { status: 404 }));
 
 export default {
-	fetch: (request, env, ctx) =>
-		router
-			.handle(request, env, ctx)
+    fetch: (request, env, ctx) =>
+        router
+            .handle(request, env, ctx)
 
-			// catch any errors
-			.catch(e => {
-				console.error(e);
-				return error(e);
-			})
+            // catch any errors
+            .catch(e => {
+                console.error(e);
+                return error(e);
+            })
 
-			// add CORS headers to all requests,
-			// including errors
-			.then(corsify),
+            // add CORS headers to all requests,
+            // including errors
+            .then(corsify),
 };
